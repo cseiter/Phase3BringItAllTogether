653fb3ca147c5512a29fa7289d455b905d01afff
chris <cseiter@gmail.com>
2021-07-24 19:31:58 -0400

    drop_table pass

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,15 +9,22 @@ class Dog
     end
 
     def self.create_table
-    sql = <<-SQL
-    create table if not exists dogs(
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        breed TEXT
+        sql = <<-SQL
+        create table if not exists dogs(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            breed TEXT
+        )
+        SQL
+        DB[:conn].execute(sql)
+    end
+    
+    def self.drop_table
+        sql = <<-SQL
+        drop table if exists dogs
+        SQL
+        DB[:conn].execute(sql)
+    end
 
-    )
-    SQL
-    DB[:conn].execute(sql)
-end
 
 end
