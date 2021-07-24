08cae7ec0ea126917d455a7ac757753b0087f3db
chris <cseiter@gmail.com>
2021-07-24 19:29:51 -0400

    create Dog class and pass first tests

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
 
+    attr_accessor :id, :name, :breed
+
+    def initialize(id: nil, name:, breed:)
+        @id=id
+        @name=name
+        @breed=breed
+    end
+
+    def self.create_table
+    sql = <<-SQL
+    create table if not exists dogs(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+
+    )
+    SQL
+    DB[:conn].execute(sql)
+end
+
 end
